349cfae962b23e27515e675ae4f5be64cbdc58b7
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-23 21:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+function downloadImages() {
+
+ let promises = images.map((val)=>{
+			
+		   let prom = new Promise((resolve,reject)=>{
+			   const img = new Image();
+			   img.src = val.url;
+			   img.alt = val.alt;
+ 			img.onload = ()=>{
+				resolve(img);
+			}
+
+			img.onerror = ()=>{
+				reject(`Failed to load image's URL: ${val.url}`)
+			}
+		});
+	 return prom;
+	});
+
+	 let x = Promise.all(promises)
+	x.then((data) =>{
+		data.forEach((image) =>{
+			output.appendChild(image);
+		})
+	})
+	x.catch((data)=>
+		{
+			console.log(data);
+		});
+
+}
+
+
+btn.addEventListener("click" , downloadImages);
